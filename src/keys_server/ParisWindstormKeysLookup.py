d8928b202966649ff4ab41981d7f3d9510a50fcd
maxwellflitton <maxwellflitton@gmail.com>
2021-06-09 17:24:24 +0100

    adding KeyLookupInterface to ParisWindstormKeysLookup

--- src/keys_server/ParisWindstormKeysLookup.py
+++ src/keys_server/ParisWindstormKeysLookup.py
@@ -2,67 +2,105 @@ __all__ = [
     'ParisWindstormKeysLookup'
 ]
 
-# Python library imports
-import io
 import logging
 import os
+from typing import Dict, Any, Optional
+
 import pandas as pd
 
-# oasislmf imports
-from oasislmf.preparation.lookup import OasisBaseKeysLookup
+from oasislmf.lookup.interface import KeyLookupInterface
 from oasislmf.utils.log import oasis_log
 from oasislmf.utils.status import OASIS_KEYS_STATUS
+from pandas import DataFrame
 
 logger = logging.getLogger()
 
-class ParisWindstormKeysLookup(OasisBaseKeysLookup):
-    """
-    Model-specific keys lookup logic.
-    """
 
+class ParisWindstormKeysLookup(KeyLookupInterface):
+    """
+    This class is responsible for Model-specific keys lookup logic.
+
+    Attributes:
+        config_dict (Dict[str, str]): parameters for the model parameters and data paths
+        config_dir (str): the directory to the configuration file
+        output_dir (str): the directory for output data
+        user_data_dir (Optional[str]): the directory pointing to the user data
+        df_ap_dict (DataFrame): Data marrying up the area peril id, latitude and longitude
+        df_vuln_dict (DataFrame): Data marrying up the vulnerability id with the construction code
+    """
     @oasis_log()
-
-    def __init__(self, keys_data_directory=None, supplier='OasisLMF', model_name='ParisWindstorm', model_version=None,
-            complex_lookup_config_fp=None,output_directory=None):
+    def __init__(self, config_dict: Dict[str, str], config_dir: str, output_dir: str,
+                 user_data_dir: Optional[str] = None) -> None:
         """
-        Initialise the static data required for the lookup.
+        The constructor for the ParisWindstormKeysLookup class.
+
+        @param config_dict: (Dict[str, str]) configuration for model parameters such as data paths and model meta data
+        @param config_dir: (str) the directory pointing to the configuration file
+        @param output_dir: (str) the directory for output data
+        @param user_data_dir: (Optional[str])
         """
-        super(self.__class__, self).__init__(
-            keys_data_directory,
-            supplier,
-            model_name,
-            model_version,
-            complex_lookup_config_fp,
-            output_directory
-        )
+        self.config_dict: Dict[str, str] = config_dict
+        self.config_dir: str = config_dir
+        self.output_dir: str = output_dir
+        self.user_data_dir: Optional[str] = user_data_dir
+        self.df_ap_dict: DataFrame = pd.read_csv(self.area_peril_data_dir)
+        self.df_vuln_dict: DataFrame = pd.read_csv(self.vulnerability_data_dir)
 
-        self.df_ap_dict=pd.read_csv(os.path.join(keys_data_directory,'areaperil_dict.csv'))
-        self.df_vuln_dict=pd.read_csv(os.path.join(keys_data_directory,'vulnerability_dict.csv'))
+    @property
+    def area_peril_data_dir(self) -> str:
+        data_path: str = str(os.path.join(self.config_dict["keys_data_path"], 'areaperil_dict.csv'))
+        if not os.path.isfile(data_path):
+            raise FileExistsError(f"data file {data_path} does not exist")
+        return data_path
 
-    def get_areaperil(self,loc_df):
+    @property
+    def vulnerability_data_dir(self) -> str:
+        data_path: str = str(os.path.join(self.config_dict["keys_data_path"], 'vulnerability_dict.csv'))
+        if not os.path.isfile(data_path):
+            raise FileExistsError(f"data file {data_path} does not exist")
+        return data_path
+
+    def get_areaperil(self, loc_df: DataFrame) -> DataFrame:
+        """
+        Processes the 'latitude' and 'longitude' before merging the area peril data.
+
+        @param loc_df: (DataFrame) the data frame to be processed and returned
+        @return: (DataFrame) data with processed 'latitude' and 'longitude' and merged area peril data
+        """
         loc_df['lat_join'] = (loc_df['latitude']*1000000).astype('int')
         loc_df['lon_join'] = (loc_df['longitude']*1000000).astype('int')
-        loc_df = loc_df.merge(self.df_ap_dict,how='left')
+        loc_df = loc_df.merge(self.df_ap_dict, how='left')
         loc_df['areaperil_id'].fillna(0)
         loc_df['ap_status'] = OASIS_KEYS_STATUS['success']['id']
         loc_df['ap_message'] = ''
         return loc_df
 
-    def get_vulnerbaility(self,loc_df):
-        loc_df = loc_df.merge(self.df_vuln_dict,how='left')
+    def get_vulnerbaility(self, loc_df: DataFrame) -> DataFrame:
+        """
+        Fills in the null 'vulnerability_id' data and merge vulnerability data with data frame.
+
+        @param loc_df: (DataFrame) the data frame to be processed
+        @return: (DataFrame) the processed data frame
+        """
+        loc_df = loc_df.merge(self.df_vuln_dict, how='left')
         loc_df['vulnerability_id'].fillna(0)
         loc_df['v_status'] = OASIS_KEYS_STATUS['success']['id']
         loc_df['v_message'] = ''
         return loc_df
 
     @oasis_log()
-    def process_locations(self,loc_df):
+    def process_locations(self, loc_df: DataFrame) -> Dict[str, Any]:
+        """
+        A generator that produces a row from the area peril data and vulnerabilities.
 
+        @param loc_df: (DataFrame) the data frame to processed and returned
+        @return: (Dict[str, Any]) row of the processed data frame produced by a yield
+        """
         loc_df.columns = map(str.lower, loc_df.columns)
-        loc_df = self.get_areaperil(loc_df)
-        loc_df = self.get_vulnerbaility(loc_df)
+        loc_df: DataFrame = self.get_areaperil(loc_df)
+        loc_df: DataFrame = self.get_vulnerbaility(loc_df)
 
-        for _,loc in loc_df.iterrows():
+        for _, loc in loc_df.iterrows():
 
             loc_id = int(loc['loc_id'])
             peril = 'WTC'
