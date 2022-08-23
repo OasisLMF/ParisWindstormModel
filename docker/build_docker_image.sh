85724a2d6410061137f0a441b0408d715c611112
benhayes21 <ben.hayes@oasislmf.org>
2022-08-23 10:42:25 +0100

    sudo for docker - to be fixed

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -1,4 +1,5 @@
-docker build -f Dockerfile.paris_windstorm_worker -t paris_windstorm_worker.1_26_2 .
+sudo docker build -f Dockerfile.paris_windstorm_worker -t paris_windstorm_worker.1_26_2 .
 
+sudo docker network create shiny-net
 
-docker-compose -f docker-compose.yml -f docker-compose-paris-windstorm.yml -f docker-compose-ui.yml up -d
+sudo docker-compose -f docker-compose.yml -f docker-compose-paris-windstorm.yml -f docker-compose-ui.yml up -d
