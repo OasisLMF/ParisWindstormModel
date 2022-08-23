2f80e828e27299444e41895765262d33e22d64e9
Charlie Farrar <charlie.farrar12@gmail.com>
2022-08-23 16:38:30 +0100

    Adder docker pull UI

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,5 +1,7 @@
 sudo docker build -f docker/Dockerfile.paris_windstorm_worker -t coreoasis/model_worker_paris_windstorm:1.26.2 .
 
+sudo docker pull coreoasis/oasisui_app:1.11.3
+
 sudo docker network create shiny-net
 
 sudo docker-compose -f docker/docker-compose.yml -f docker/docker-compose-paris-windstorm.yml -f docker/docker-compose-ui.yml up -d
